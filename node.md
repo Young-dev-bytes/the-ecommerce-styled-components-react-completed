af11fe00ae64439c7037e5aa84454ace34c06c36
Young <18855490639@163.com>
2024-08-04 23:39:59 +0800

    first commit

---
+++ node.md
@@ -0,0 +1,5 @@
+npm i styled-components
+
+npm install @material-ui/core @material-ui/icons
+
+npm install @mui/icons-material
---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import "./App.css";
+import Home from "./pages/Home";
+import ProductList from "./pages/ProductList";
+import Product from "./pages/Product";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import Cart from "./pages/Cart";
+
+function App() {
+  return (
+    <div className="App">
+      {/* <Home /> */}
+      {/* <ProductList /> */}
+      {/* <Product /> */}
+      {/* <Register /> */}
+      {/* <Login /> */}
+      <Cart />
+    </div>
+  );
+}
+
+export default App;
